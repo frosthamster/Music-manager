c9dbe8c8830960456fa37e1618a36cdf705453d8
Unknown <frosthamster@gmail.com>
2018-06-12 13:54:26 +0500

    Added progress bar update frequency

--- utils.py
+++ utils.py
@@ -16,24 +16,29 @@ def get_size(start_path='.'):
 
 
 class ProgressTracker:
-    def __init__(self, total_size, msg):
+    def __init__(self, total_size, msg, update_frequency=2.1):
         self._pb = ui.get_progress_bar(msg)
-        self._current = 0
+        self._current_size = 0
         self._total_size = total_size
+        self._next_update_border = 0
+        self._update_frequency = update_frequency
         self._closed = False
 
     def add_progress(self, count):
         if self._closed:
             return
-        self._current += count
-        percent = self._current / self._total_size * 100
+        self._current_size += count
+        percent = self._current_size / self._total_size * 100
         if percent > 99:
             return
-        self._pb.print_progress_bar(percent)
+        if percent >= self._next_update_border:
+            self._next_update_border = max(self._next_update_border + self._update_frequency,
+                                           percent // self._update_frequency + self._update_frequency)
+            self._pb.print_progress_bar(percent)
 
     def reset(self, total_size=None, msg=None):
         self._closed = False
-        self._current = 0
+        self._current_size = 0
         if msg is not None:
             self._pb = ui.get_progress_bar(msg)
         if total_size is not None:
